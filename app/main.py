c1ae2bde05409f0c63266b6f8281426a0d6e49a0
MattiasZurkovic <mattias@zurkovic.com>
2015-07-27 11:21:52 +0200

    Add some sample URLs

--- app/main.py
+++ app/main.py
@@ -8,40 +8,30 @@ from hashlib import sha1
 
 app = Flask(__name__)
 
+
+# URLs
+# 'http://www.forbes.com/real-time/feed2/'         ##### Forbes - Lastest Headlines
+#
+
+
+
+# The index method and page will show the TOP(most popular) news stories from all the sources(i.e. CNN, Forbes, BBC, etc.)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    TOP_URLS = ['http://www.forbes.com/feeds/popstories.xml',     ##### Forbes - Most Popoular
+                'http://rss.cnn.com/rss/edition.rss',             ##### CNN - Most Popular
 
-@app.route('/valve')
-def valve():
-    ##### RSS Feed Urls:
-    VALVE_URLS = ['http://store.steampowered.com/feeds/news.xml'     # Steam URL
-                  'http://blog.counter-strike.net/index.php/feed/',  # CSGO URL
-                  'http://blog.dota2.com/feed/',                     # DOTA URL
-                  'http://www.teamfortress.com/rss.xml',             # TF2 URL
-                  'http://www.sourcefilmmaker.com/rss.xml'           # Source Film Maker URL
-                  ]
+                ]
 
     ##### Valve Feed
-    valve_entries = []
-    for url in VALVE_URLS:
-        valve_entries.extend(feedparser.parse(url).entries)
+    entries = []
+    for url in TOP_URLS:
+        entries.extend(feedparser.parse(url).entries)
 
-    valve_entries_sorted_reverse = sorted(valve_entries, key=lambda e: e.published_parsed, reverse=True)
-    valve_entries_sorted_oldest = sorted(valve_entries, key=lambda e: e.published_parsed, reverse=False)
+    all_entries_sorted = sorted(entries, key=lambda e: e.published_parsed, reverse=True)
 
-    return render_template('valve.html', entries=valve_entries_sorted_reverse, reg_entries=valve_entries_sorted_oldest)
+    return render_template('index.html', entries=all_entries_sorted)
 
-
-@app.route('/test_feed')
-def test_feed():
-    return render_template('test.html', csgo_html = csgo_link_parse())
-
-
-def csgo_link_parse():
-    csgo_url = feedparser.parse('http://blog.counter-strike.net/index.php/feed/')
-    for post in csgo_url.entries:
-        return post.title
 
 if __name__ == '__main__':
     app.run()
